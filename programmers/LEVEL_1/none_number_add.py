5fa192a20a530073d8ea28c83798820815c9fbbb
GyuTaeKim0729 <gyutae0729@gmail.com>
2021-12-28 10:49:57 +0900

    [Grow Up] 1.1.3

--- programmers/LEVEL_1/none_number_add.py
+++ programmers/LEVEL_1/none_number_add.py
@@ -1,5 +1,16 @@
-if __name__ == '__main__':
-    result = 0
-    solution_result = 0
-    print("Success" if result == solution_result else "Fail")
+def solution(numbers):
+    left_numbers = [x for x in range(0, 10)]
+    for i in numbers:
+        left_numbers.remove(i)
+    return sum(left_numbers)
 
+
+def solution2(numbers):
+    return sum(range(10)) - sum(numbers)
+
+
+if __name__ == '__main__':
+    numbers = [1, 2, 3, 4, 6, 7, 8, 0]
+    result = 14
+    solution_result = solution2(numbers)
+    print("Success" if result == solution_result else "Fail")
